7a0e27701c253786fa3d46eb459d919ab53a6668
Tanishk Kumar <kumartanishk216@gmail.com>
2024-12-14 12:08:59 +0530

    Add files via upload

---
+++ list.methods.py
@@ -0,0 +1,9 @@
+friends =["Apple", "Orange", 5, 345.07, False, "Tanishk"]
+print(friends)
+friends.append("laptop")
+print(friends)
+
+l1 = [1,34,67,2,24,45]
+#l1.sort()
+l1.insert(3,4444)
+print(l1)
---
+++ list.py
@@ -0,0 +1,6 @@
+friends =["Apple", "Orange", 5, 345.07, False, "Tanishk"]
+
+print(friends[5])
+friends[5] = "Grapes"  #unlike Strings lists are mutable
+
+print(friends[5])
